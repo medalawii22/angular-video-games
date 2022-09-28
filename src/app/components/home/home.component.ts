3fba13aaf66357471607b05f7a6abff5bad697b2
unknown <medalawii222@gmail.com>
2022-09-28 16:25:37 +0000

    third commit

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { subscriptionLogsToBeFn } from 'rxjs/internal/testing/TestScheduler';
+import { APIResponse, Game } from 'src/app/models';
+import { HttpService } from 'src/app/services/http.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  public sort!: string ; 
+  public games: Array<Game> | undefined;
+  public routeSub!:Subscription;
+  public gameSub!: Subscription;
+
+  constructor(
+    private httpService:HttpService,
+    private router:Router,
+    private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params:Params)=>{
+      if(params['game-search']){
+        this.searchGames('metacrit',params['game-search']);
+      }else{
+        this.searchGames('metacrit');
+      }
+    });
+  }
+
+  searchGames(sort:string,search?:string):void{
+    this.gameSub = this.httpService
+        .getGameList(sort,search)
+        .subscribe((gameList : APIResponse<Game>)=>{
+          this.games=gameList.results;
+          console.log(gameList);
+        });
+  }
+  openGameDetails(id:string){
+    this.router.navigate(['details',id]);
+  }
+  ngOnDestroy():void{
+    if(this.gameSub){
+      this.gameSub.unsubscribe();
+    }
+    if(this.routeSub){
+      this.routeSub.unsubscribe();
+    }
+  }
+
+}
---
+++ src/app/interceptors/http-errors.interceptor.ts
@@ -0,0 +1,19 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { catchError, Observable, throwError } from "rxjs";
+
+@Injectable()
+export class HttpErrorsInterceptor implements HttpInterceptor{
+    constructor(){}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(req).pipe(
+            catchError((err)=>{
+                console.log(err);
+                return throwError(()=>err);
+            })
+        );
+    }
+}
+
+
